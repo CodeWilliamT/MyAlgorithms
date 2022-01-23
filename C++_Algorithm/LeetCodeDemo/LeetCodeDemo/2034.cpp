afa343d0ae9c1a152b8a710a121aae3944cbdd2b
William Tong <624573425@qq.com>
2022-01-23 16:47:16 +0800

    +2034

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/2034.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/2034.cpp
@@ -1,67 +1,93 @@
 ﻿using namespace std;
-#include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <set>
 #include <map>
-//设计题
-//哈希
+#include <unordered_map>
+//设计题 哈希 新解
 class StockPrice {
 private:
-    int mx, mn, cur;
-    bool isInit;
-    map<int, int> flow;
-    multiset<int> dt;
+    unordered_map<int, int> ump;
+    map<int, int> mp;
+    int cur = 0;
 public:
     StockPrice() {
-        mx = 1;
-        mn = 1000000001;
-        cur = -1;
-        isInit = false;
-        flow.clear();
-        dt.clear();
+        ump.clear();
+        mp.clear();
+        cur = 0;
     }
 
     void update(int timestamp, int price) {
-        if (flow.count(timestamp))
-        {
-            int prevPrice = flow[timestamp];
-            dt.erase(dt.find(prevPrice));
+        if (ump.count(timestamp)) {
+            --mp[ump[timestamp]];
+            if (mp[ump[timestamp]] == 0) {
+                mp.erase(ump[timestamp]);
+            }
+            ump.erase(timestamp);
         }
-        flow[timestamp] = price;
-        dt.insert(price);
-        if (!isInit)
-        {
-            mx = price;
-            mn = price;
-            cur = price;
-            isInit = true;
-            return;
-        }
-        mx = *prev(dt.end());
-        mn = *dt.begin();
-        cur = (*prev(flow.end())).second;
+        mp[price]++;
+        ump[timestamp]= price;
+        cur = max(timestamp,cur);
     }
 
     int current() {
-        return cur;
+        return ump[cur];
     }
 
     int maximum() {
-        return mx;
+        return (*prev(mp.end())).first;
     }
 
     int minimum() {
-        return mn;
+        return (*mp.begin()).first;
     }
 };
 
-/**
- * Your StockPrice object will be instantiated and called as such:
- * StockPrice* obj = new StockPrice();
- * obj->update(timestamp,price);
- * int param_2 = obj->current();
- * int param_3 = obj->maximum();
- * int param_4 = obj->minimum();
- */
+//设计题 哈希
+//class StockPrice {
+//private:
+//    int mx, mn, cur;
+//    bool isInit;
+//    map<int, int> flow;
+//    multiset<int> dt;
+//public:
+//    StockPrice() {
+//        mx = 1;
+//        mn = 1000000001;
+//        cur = -1;
+//        isInit = false;
+//        flow.clear();
+//        dt.clear();
+//    }
+//
+//    void update(int timestamp, int price) {
+//        if (flow.count(timestamp))
+//        {
+//            int prevPrice = flow[timestamp];
+//            dt.erase(dt.find(prevPrice));
+//        }
+//        flow[timestamp] = price;
+//        dt.insert(price);
+//        if (!isInit)
+//        {
+//            mx = price;
+//            mn = price;
+//            cur = price;
+//            isInit = true;
+//            return;
+//        }
+//        mx = *prev(dt.end());
+//        mn = *dt.begin();
+//        cur = (*prev(flow.end())).second;
+//    }
+//
+//    int current() {
+//        return cur;
+//    }
+//
+//    int maximum() {
+//        return mx;
+//    }
+//
+//    int minimum() {
+//        return mn;
+//    }
+//};
