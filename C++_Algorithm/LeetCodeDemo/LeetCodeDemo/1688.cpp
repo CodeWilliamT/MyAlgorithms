517e4dad4dc0bf6ba6c196904966de3e0aeb9122
WeiliangTong <624573425@qq.com>
2022-01-25 12:26:44 +0800

    +1688

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/1688.cpp
@@ -0,0 +1,12 @@
+﻿//模拟 简单题
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int rst = 0;
+        while (n != 1) {
+            rst += n / 2;
+            n = n / 2 + n % 2;
+        }
+        return rst;
+    }
+};
