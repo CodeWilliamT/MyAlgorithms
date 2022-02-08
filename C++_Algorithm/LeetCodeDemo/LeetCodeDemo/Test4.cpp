3e2d0caa99e3fc69255d0841e18cf153ae11e2b1
WeiliangTong <624573425@qq.com>
2022-02-08 19:03:19 +0800

    +test4

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/Test4.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/Test4.cpp
@@ -10,3 +10,17 @@
 #include <queue>
 #include <stack>
 #include <functional>
+//贪心
+//左右左右跳着拿，拿光，看拿最后用的代价。
+//左右左右
+class Solution {
+public:
+    int minimumTime(string s) {
+        int n = s.size();
+        vector<int> rst(n, 2);
+        rst[0]=rst[n-1] = 1;
+        for (int i = 1;i<n;i++) {
+            c = rst;
+        }
+    }
+};
