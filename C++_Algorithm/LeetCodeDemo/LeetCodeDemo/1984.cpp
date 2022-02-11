87f61703403f4c5a85a96c7ddfb4c59223c05092
William Tong <624573425@qq.com>
2022-02-12 00:32:54 +0800

    +1984

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/1984.cpp
@@ -0,0 +1,16 @@
+﻿using namespace std;
+#include <vector>
+#include <algorithm>
+//简单题
+//排序后 获取nums[i] - nums[i - k + 1]的最小值。
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int rst = nums[k - 1]- nums[0];
+        for (int i = k; i < nums.size(); i++) {
+            rst = min(rst,nums[i] - nums[i - k + 1]);
+        }
+        return rst;
+    }
+};
