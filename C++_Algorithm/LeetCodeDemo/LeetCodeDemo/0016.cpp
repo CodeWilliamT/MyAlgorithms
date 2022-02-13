2faf779e50bd86bd903a834eb6575727878f4e2c
William Tong <624573425@qq.com>
2022-02-13 17:06:09 +0800

    +0016

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0016.cpp
@@ -0,0 +1,33 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <functional>
+//哈希 
+//求任意俩数和，并保存为哈希mp,俩数和为key，指向两数索引
+//对target-nums[i]在mp里做两分查找，如果mp[x].count(i)<mp[x].size()/2。
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        unordered_map<int, multiset<int>> mp;
+        int n = nums.size();
+        int sum;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                sum = nums[i] + nums[j];
+                mp[sum].insert(i);
+                mp[sum].insert(j);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+};
---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/1189.cpp
@@ -0,0 +1,15 @@
+﻿using namespace std;
+#include <iostream>
+//哈希
+//统计各个字母出现数，答案为balloon单词内出现最缺的字母的次数。
+//min({ l[0],l[1],l['l'-'a']/2,l['o' - 'a']/2,l['n' - 'a'] });
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int l[26]{};
+        for (char& c : text) {
+            l[c-'a']++;
+        }
+        return min({ l[0],l[1],l['l'-'a']/2,l['o' - 'a']/2,l['n' - 'a'] });
+    }
+};
