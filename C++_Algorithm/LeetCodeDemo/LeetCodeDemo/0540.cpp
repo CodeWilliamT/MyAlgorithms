34963734a37894c620c0a0d5e3171d9fb5e0816d
WeiliangTong <624573425@qq.com>
2022-02-14 19:09:15 +0800

    +0540

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0540.cpp
@@ -0,0 +1,20 @@
+﻿using namespace std;
+#include <vector>
+//两分查找 找规律
+//若m是奇数那么跟前一个相等，或m是偶数跟后一个相等则表示这段正常，否则就不正常
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1, m = (l + r) / 2;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (m % 2 && nums[m] == nums[m - 1] || m % 2 == 0 && nums[m] == nums[m + 1]) {
+                l = m + 1;
+            }
+            else {
+                r = m;
+            }
+        }
+        return nums[l];
+    }
+};
