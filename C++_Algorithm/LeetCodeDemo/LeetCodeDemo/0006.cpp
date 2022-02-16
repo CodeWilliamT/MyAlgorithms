6125f9b06582daf2d51038976a0e01dfcd852337
WeiliangTong <624573425@qq.com>
2022-02-16 20:07:31 +0800

    +0006

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0006.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0006.cpp
@@ -1,18 +1,30 @@
 ﻿using namespace std;
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <unordered_set>
-#include <unordered_map>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <functional>
+//模拟
 class Solution {
 public:
     string convert(string s, int numRows) {
-
+        int n = s.size();
+        vector<string>  p(numRows);
+        string rst;
+        int cnt = 0, pa = cnt + numRows, pb = pa + numRows - 2;
+        for (int i = 0; i < n; i++) {
+            if (i < pa) {
+                p[i - cnt].push_back(s[i]);
+            }
+            else if (i < pb) {
+                p[numRows - 1 - (i - (pa - 1))].push_back(s[i]);
+            }
+            else {
+                cnt = i;
+                pa = cnt + numRows, pb = pa + numRows - 2;
+                p[i - cnt].push_back(s[i]);
+            }
+        }
+        for (auto& e : p) {
+            rst += e;
+        }
+        return rst;
     }
 };
