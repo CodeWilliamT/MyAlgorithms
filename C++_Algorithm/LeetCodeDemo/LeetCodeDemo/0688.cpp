5620e98c1d192069ad01e8e1811e9e93e50c2f82
WeiliangTong <624573425@qq.com>
2022-02-17 17:52:38 +0800

    +688

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0688.cpp
@@ -0,0 +1,44 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <functional>
+//回溯(深搜) 记忆化搜索 动态规划
+class Solution {
+public:
+    double knightProbability(int n, int k, int row, int column) {
+        double rst=1;
+        int dir[8][2] = { {1,2},{2,1},{2,-1},{1,-2},{-1,2},{-2,1},{-2,-1}, {-1,-2} };
+        bool v[25][25]{};
+        int f[25][25]{};
+        function<bool(int, int, int)> dfs = [&](int x, int y, int step) {
+            if (x < 0 || y < 0 || x >= n || y >= n) {
+                return false;
+            }
+            if (v[x][y])
+            {
+                rst -= f[x][y];
+                return true;
+            }
+            v[x][y] = 1;
+            if (step >= k) {
+                return true;
+            }
+            int cnt=0;
+            for (int i = 0; i < 8; i++) {
+                cnt+=dfs(x + dir[i][0], y + dir[i][1], step + 1);
+            }
+            f[x][y] = 1-cnt / 8;
+            return true;
+        };
+        dfs(row, column, 0);
+        return rst;
+    }
+};
