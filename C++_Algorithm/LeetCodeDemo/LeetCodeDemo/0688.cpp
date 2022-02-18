f4da645908c3095077e2b64b89a221b135534915
William Tong <624573425@qq.com>
2022-02-18 23:51:48 +0800

    +688

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0688.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0688.cpp
@@ -10,35 +10,33 @@
 #include <queue>
 #include <stack>
 #include <functional>
-//回溯(深搜) 记忆化搜索 动态规划
+//动态规划
+//步骤，位置，指向概率 f[101][25][25]{}
+//答案为k步的概率和。
+//f[t][x][y] += f[t - 1][i][j] / 8;
 class Solution {
 public:
     double knightProbability(int n, int k, int row, int column) {
-        double rst=1;
+        if (!k)return 1;
+        double rst = 0;
         int dir[8][2] = { {1,2},{2,1},{2,-1},{1,-2},{-1,2},{-2,1},{-2,-1}, {-1,-2} };
-        bool v[25][25]{};
-        int f[25][25]{};
-        function<bool(int, int, int)> dfs = [&](int x, int y, int step) {
-            if (x < 0 || y < 0 || x >= n || y >= n) {
-                return false;
+        double f[101][25][25]{};
+        f[0][row][column] = 1;
+        for (int t = 1; t <= k; t++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int q = 0; q < 8; q++) {
+                        int x = i + dir[q][0];
+                        int y = j + dir[q][1];
+                        if (x < 0 || y < 0 || x >= n || y >= n) {
+                            continue;
+                        }
+                        f[t][x][y] += f[t - 1][i][j] / 8;
+                        if (t == k)rst += f[t - 1][i][j] / 8;
+                    }
+                }
             }
-            if (v[x][y])
-            {
-                rst -= f[x][y];
-                return true;
-            }
-            v[x][y] = 1;
-            if (step >= k) {
-                return true;
-            }
-            int cnt=0;
-            for (int i = 0; i < 8; i++) {
-                cnt+=dfs(x + dir[i][0], y + dir[i][1], step + 1);
-            }
-            f[x][y] = 1-cnt / 8;
-            return true;
-        };
-        dfs(row, column, 0);
+        }
         return rst;
     }
 };
