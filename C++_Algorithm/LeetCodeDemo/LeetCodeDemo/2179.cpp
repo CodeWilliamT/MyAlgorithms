45d1a915a1058d5b4554b1c334cf203630aa97f2
WeiliangTong <624573425@qq.com>
2022-02-22 19:30:38 +0800

    +2179

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/2179.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/2179.cpp
@@ -10,31 +10,28 @@
 #include <queue>
 #include <stack>
 #include <functional>
-//树状数组 巧思
+//树状数组 置换 巧思
 //找三个值，在两个数组中位置都是递增的。
 //转换出nums2值映射索引的数组mp,比nums2[i]索引大的数目为n-mp[nums2[i]]。
 class Solution {
 public:
     long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        vector<int> mp1(n),mp2(n);
-        for (int i = 0; i < n; i++) {
-            mp1[nums1[i]] = i;
-        }
-        for (int i = 0; i < n; i++) {
-            mp2[i]= mp1[nums2[i]];
-        }
-        long long less;
-        long long rst = 0;
-        for (int i = 1; i < n-1; i++) {
+        vector<int> p(n);
+        for (int i = 0; i < n; ++i)
+            p[nums1[i]] = i;
+        long long ans = 0;
+        vector<int> tree(n + 1);
+        long long less = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            for (int j = p[nums2[i - 1]] + 1; j <= n; j += j & -j) // 将 p[nums2[i-1]]+1 加入树状数组
+                ++tree[j];
+            int y = p[nums2[i]];
             less = 0;
-            for (int j = 0; j < i; j++) {
-                if (mp2[j] < mp2[i]) {
-                    less++;
-                }
-            }
-            rst += less * (n - 1-mp2[i] - (i-less));
+            for (int j = y; j; j &= j - 1) // 计算 less
+                less += tree[j];
+            ans += less * (n - 1 - y - (i - less));
         }
-        return rst;
+        return ans;
     }
 };
