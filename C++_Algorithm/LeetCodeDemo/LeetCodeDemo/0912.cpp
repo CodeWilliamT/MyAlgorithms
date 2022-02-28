e23a398de8d1052308226aff75beb41e9544a909
WeiliangTong <624573425@qq.com>
2022-02-28 15:14:25 +0800

    add vedios

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0912.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0912.cpp
@@ -1,6 +1,6 @@
 ﻿using namespace std;
 #include <vector>
-//快速排序
+//快速排序 O(NlogN) O(1)
 //取区间随机一位置x=rand()%(R+1-L)+L，v=nums[x]作比较基准。(否则,比如取第一个数，当数组递增时复杂度为n*n);
 //令l = L, r = R, 找个左侧出发找到比该数nums[x]大的nums[l]，右侧出发找到比该数nums[x]小的nums[l]，找到的l <= r, 则交换俩数，直到l大于r结束循环。
 //对nums[L, r], nums[l, R]做1，2。
@@ -33,7 +33,8 @@ public:
     }
 };
 
-//堆排序
+//堆排序 O(NlogN) O(1)
+//n/2到0维护顶堆构造出大顶堆(增序)，交换顶底，从n-2到1,一直做 维护顶堆，完事再交换顶底。
 class Solution {
 public:
     //从根节点出发的维护操作
@@ -42,17 +43,14 @@ public:
         int maxIdx = i;
         if (l <= x && a[l] > a[i])maxIdx = l;//增序则>建大顶堆,降序则<建小顶堆,找第k大则>建大顶堆k次,a[0]就是。
         if (r <= x && a[r] > a[maxIdx])maxIdx = r;
-        if (maxIdx != i)
-        {
+        if (maxIdx != i){
             swap(a[maxIdx], a[i]);
             HeapifyNode(a, maxIdx, x);
         }
     }
     //遍历根节点的构建顶堆操作
     void HeapifyTree(vector<int>& a, int x) {
-        int n = (x - 1) >> 1;
-        for (int i = n; i > -1; i--)
-        {
+        for (int i = x/2; i >=0; i--){
             HeapifyNode(a, i, x);
         }
     }
@@ -62,8 +60,7 @@ public:
         int n = a.size();
         HeapifyTree(a, n - 1);
         swap(a[0], a[n - 1]);
-        for (int i = n - 2; i > 0; i--)
-        {
+        for (int i = n - 2; i > 0; i--){
             HeapifyNode(a, 0, i);
             swap(a[0], a[i]);
         }
--- README.md
+++ README.md
@@ -2,19 +2,18 @@
 
 This repo contains some my personal small develop projects, toolkits and Leetcode algorithm codes with my pesonal solution comments.
 
-这个Repo主要包含了个人日常练习算法做的leetcode题目的代码、做题时的思路注释，以及一些个人开发的小软件、工具。
+这个Repo主要包含了个人日常练习算法做的leetcode题目的代码、做题时的思路注释，以及一些个人自用开发的小软件、工具。
 
-包括:
+主要包括:
 
 LeetCodeDemo:个人日常Leetcode算法做题Repo。注释包含自身的处理思路。
+
+MyMachineControl_Toolkits:一些硬件控制跟图像处理开发编写使用的类库和一些Demo,C#。
 
 DoubleSubSwitcher:一个简单的双语字幕上下行交换器,C#winform。
 
 DoBinaryMap:一个二维码识别器,C#winform。
 
-MyMachineControl_Toolkits:一些硬件控制跟图像处理开发编写使用的类库和一些Demo,C#。
-
-StackAnalysisDemo:股票形态分析Demo，python+C#winform，还没写。
-
 Python_BaoStock_Demo:一个股票特征形态筛选的简易脚本,python。
 
+Samples\Scan_Machine_Working_Videos:AOI检测机的功能测试的Record Vedio。
