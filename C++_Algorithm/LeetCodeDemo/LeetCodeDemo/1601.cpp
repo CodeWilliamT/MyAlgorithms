bd66a5b4cbba179cf33beab0b53c0382c810fe49
WeiliangTong <624573425@qq.com>
2022-02-28 19:09:51 +0800

    +1601

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/1601.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/1601.cpp
@@ -1,20 +1,37 @@
 ﻿using namespace std;
-#include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <unordered_set>
-#include <unordered_map>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <functional>
-//图
-//各点出度入度相等
+//枚举
+//小数据 共计2^16，枚举约64000个状态 判定是否合法
+//判定各个状态时各点的入度出度是否相等
 class Solution {
 public:
-    int maximumRequests(int n, vector<vector<int>>& requests) {
-
+    int maximumRequests(int n, vector<vector<int>>& r) {
+        int len = r.size();
+        int total = 1 << len;
+        int in[21]{};
+        int rst=0;
+        int cnt = 0;
+        bool flag;
+        for (int i = 0; i < total; i++) {
+            cnt = 0;
+            for (int j = 0; j < len; j++) {
+                if (i >> j & 1) {
+                    in[r[j][0]]++;
+                    in[r[j][1]]--;
+                    cnt++;
+                }
+            }
+            flag = 1;
+            for (int j = 0; j < n; j++) {
+                if (in[j]) {
+                    flag = 0;
+                }
+                in[j] = 0;
+            }
+            if (flag) {
+                rst = max(rst, cnt);
+            }
+        }
+        return rst;
     }
 };
