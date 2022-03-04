8161ac7b1d6e6a1910e5b7686dff1aab56de50c3
William Tong <624573425@qq.com>
2022-03-05 02:14:47 +0800

    +0521

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0521.cpp
@@ -0,0 +1,10 @@
+﻿using namespace std;
+#include <iostream>
+//巧思
+//分析下所求就知道,不同则为其较长的，相同则没。
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        return a.compare(b) == 0 ? -1 : max(a.size(), b.size());
+    }
+};
