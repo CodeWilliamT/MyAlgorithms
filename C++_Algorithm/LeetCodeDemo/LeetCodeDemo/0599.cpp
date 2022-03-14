f43d38e1a0173b0103298403bd98347e47593055
William Tong <624573425@qq.com>
2022-03-15 00:23:30 +0800

    +0599

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0599.cpp
@@ -0,0 +1,21 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+//简单模拟
+//按长度配对过呗
+class Solution {  
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        int n1 = list1.size(), n2 = list2.size();
+        vector<string> rst;
+        for (int i = 0; i < n1 + n2; i++) {
+            for (int j = max(i-n2+1,0); j < n1&&i-j>=0; j++) {
+                if (list1[j] == list2[i-j]) {
+                    rst.push_back(list1[j]);
+                }
+            }
+            if (rst.size())break;
+        }
+        return rst;
+    }
+};
