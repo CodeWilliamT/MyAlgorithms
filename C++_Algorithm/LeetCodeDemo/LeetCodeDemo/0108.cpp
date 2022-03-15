6e758e47bf069e813e51848ce674fc93280c5613
William Tong <624573425@qq.com>
2022-03-15 20:54:01 +0800

    +0108 0572

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0108.cpp
@@ -0,0 +1,31 @@
+﻿struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+//回溯 两分 树结构遍历
+//无限中点构造
+class Solution {
+	void dfs(vector<int>& nums,int l,int r, TreeNode* root) {
+		if (l > r)return;
+		int mid = (l + r) / 2;
+		root->val = nums[mid];
+		if (l <= mid - 1) {
+			root->left = new TreeNode();
+			dfs(nums, l, mid - 1, root->left);
+		}
+		if (mid + 1 <= r) {
+			root->right = new TreeNode();
+			dfs(nums, mid + 1, r, root->right);
+		}
+	}
+public:
+	TreeNode* sortedArrayToBST(vector<int>& nums) {
+		TreeNode* root=new TreeNode();
+		dfs(nums,0, nums.size() - 1,root);
+		return root;
+	}
+};
---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/0572.cpp
@@ -0,0 +1,42 @@
+﻿struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+//树结构遍历 回溯 模拟
+//遍历树，一节点val根target的val相同则遍历该节点子树比对。
+class Solution {
+	bool checkSubtree(TreeNode* root, TreeNode* subRoot) {
+		if (!root)return !subRoot;
+		if (!subRoot)return false;
+		if (root->val != subRoot->val)
+			return false;
+		bool left = 0, right = 0;
+		left = checkSubtree(root->left, subRoot->left);
+		right = checkSubtree(root->right, subRoot->right);
+		return left && right;
+	}
+	bool dfs(TreeNode* root, TreeNode* subRoot) {
+		if (!root)return !subRoot;
+		if (!subRoot)return false;
+		bool rst = false;
+		if (root->val == subRoot->val) {
+			rst = checkSubtree(root, subRoot);
+			if (rst)
+				return rst;
+		}
+		bool left = 0, right = 0;
+		left = dfs(root->left, subRoot);
+		right = dfs(root->right, subRoot);
+		return left || right;
+	}
+public:
+	bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+		if (!root)
+			return !subRoot;
+		return dfs(root, subRoot);
+	}
+};
