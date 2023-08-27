20e09041fe47dc95aa123c73f669c17787bd998b
624573425@qq.com <624573425@qq.com>
2023-08-27 22:24:08 +0800

    +test20230827

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/Test2.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/Test2.cpp
@@ -1,16 +1,19 @@
 ﻿using namespace std;
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
 #include <unordered_set>
-#include <unordered_map>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <functional>
-#include <bitset>
-typedef long long ll;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
+//哈希
+class Solution {
+public:
+    long long minimumPossibleSum(int n, int t) {
+        long long rst = 0;
+        int x=1;
+        unordered_set<int> st;
+        while (n--) {
+            while (st.count(t - x)) {
+                x++;
+            }
+            rst += x;
+            st.insert(x++);
+        }
+        return rst;
+    }
+};
