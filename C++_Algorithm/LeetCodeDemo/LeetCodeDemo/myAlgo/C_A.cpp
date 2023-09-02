f7ea67cc62643412c69a18deb5bf397fa6fa4b42
624573425@qq.com <624573425@qq.com>
2023-09-02 23:49:59 +0800

    +C_A coments

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/myAlgo/C_A.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/myAlgo/C_A.cpp
@@ -3,6 +3,8 @@ using namespace std;
 typedef long long ll;
 class C_A {
 public:
+    //组合数
+    //n个东西选m个
     ll C(int n, int m) {
         long long res = 1;
         m = min(m, n - m);
@@ -12,6 +14,8 @@ public:
         }
         return res;
     }
+    //排列数
+    //n个东西选m个且可交换顺序算不同的
     ll A(int n, int m) {
         long long res = 1;
         for (int i = 0; i < m; ++i) {
