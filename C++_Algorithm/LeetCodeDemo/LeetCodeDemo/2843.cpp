2c2708c46e84bc0948cf945f5651c74f3c751ccb
624573425@qq.com <624573425@qq.com>
2023-09-07 01:47:37 +0800

    +tests

---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/2843.cpp
@@ -0,0 +1,37 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <bitset>
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+//简单模拟
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        string s;
+        int n,delta;
+        int rst=0;
+        for (int i = low; i <= high; i++) {
+            s = to_string(i);
+            n = s.size();
+            if (n % 2)continue;
+            delta = 0;
+            for (int j = 0; j < n/2; j++) {
+                delta += s[j];
+                delta -= s[n/2+j];
+            }
+            if (!delta)rst++;
+        }
+        return rst;
+    }
+};
---
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/2844.cpp
@@ -0,0 +1,36 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <bitset>
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+//找规律 巧思
+class Solution {
+public:
+    int minimumOperations(string num) {
+        int n=num.size();
+        string s[] = {"00","25","50","75"};
+        int match[4]{};
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (num[n - 1 - i] == s[j][1-match[j]]) {
+                    if (match[j])return i-1;
+                    match[j]++;
+                }
+            }
+        }
+        if (match[0])return n-1;
+        return n;
+    }
+};
