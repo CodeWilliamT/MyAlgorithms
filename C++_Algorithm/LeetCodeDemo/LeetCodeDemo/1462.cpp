94d3ea0bb3ef5a4da5c9c2dffdbe9401fc9ac787
624573425@qq.com <624573425@qq.com>
2023-09-13 02:22:54 +0800

    +1462

--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/1462.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/1462.cpp
@@ -17,46 +17,51 @@ typedef pair<ll, ll> pll;
 typedef pair<int, int> pii;
 
 class Solution {
-    //处理特殊边界,能下一步则返回true
-    //邻接表广搜，返回抵达终点步骤数，不能则返回-1
-    bool BFS(int n, vector<vector<int>>&g, int start, int end)
-    {
-        vector<int> v(n, 0);
+    //求前后点间是否有前置关系,
+    //多少个点,前、后关系数组，是否从1开始
+    vector<vector<bool>> IsPre(int n, vector<vector<int>>& edges, bool from1 = 0) {
         queue<int> q;
-        q.push(start);
-        int witdh;
-        int cur;
+        vector<vector<int>> g(n + from1);
+        vector<int> in(n + from1);
+        vector<vector<bool>> isPre(n + from1, vector<bool>(n + from1, false));
+        for (auto& e : edges) {
+            g[e[1]].push_back(e[0]);
+            in[e[0]]++;
+        }
+        for (int i = from1; i < n + from1; i++) {
+            if (!in[i]) {
+                q.push(i);
+            }
+        }
+        int cnt = 0;
         while (!q.empty()) {
-            witdh = q.size();
-            while (witdh--) {
-                cur = q.front();
+            int len = q.size();
+            while (len--)
+            {
+                int cur = q.front();
+                //cur的顺序就是拓扑排序的顺序
+                cnt++;
                 q.pop();
-                if (v[cur]) {
-                    continue;//处理边界情况
-                }
-                v[cur] = 1;//打标记
-                if (cur == end) {//抵达终点
-                    return true;
-                }
                 for (auto& e : g[cur]) {
-                    if (v[e]) {
-                        continue;//处理边界情况
+
+                    isPre[cur][e] = true;
+                    for (int i = from1; i < n+ from1; i++) {
+                        isPre[i][e] = isPre[i][e] || isPre[i][cur];
                     }
-                    q.push(e);//加入下一步
+                    in[e]--;
+                    if (!in[e])
+                        q.push(e);
                 }
             }
         }
-        return false;
+        return isPre;
     }
 public:
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& ps, vector<vector<int>>& qs) {
-        vector<vector<int>> g(n);
-        for (auto& p : ps) {
-            g[p[0]].push_back(p[1]);
-        }
+        vector<vector<bool>> ispre = IsPre(n, ps,false);
         vector<bool> rst;
         for (auto& q : qs) {
-            rst.push_back(BFS(n, g, q[0], q[1]));
+            rst.push_back(ispre[q[0]][q[1]]);
         }
         return rst;
     }
--- C++_Algorithm/LeetCodeDemo/LeetCodeDemo/myAlgo/Map/TopoSort.cpp
+++ C++_Algorithm/LeetCodeDemo/LeetCodeDemo/myAlgo/Map/TopoSort.cpp
@@ -8,14 +8,14 @@ using namespace std;
 class TopoSort {
 public:
     //求是否无环
-    //多少个点（点的最大消耗）,前、后关系数组，是否从1开始
-    bool IsNoCircle(int n, vector<vector<int>>& edges, bool from1 = true) {
+    //多少个点,后、前关系数组，是否从1开始
+    bool IsNoCircle(int n, vector<vector<int>>& edges, bool from1 = 0) {
         queue<int> q;
         vector<vector<int>> g(n + from1);
         vector<int> in(n + from1);
         for (auto& e : edges) {
-            g[e[1]].push_back(e[0]);
-            in[e[0]]++;
+            g[e[1]].push_back(e[0]);//前置点加与后置点的关系。
+            in[e[0]]++;//后置点加入度
         }
         for (int i = from1; i < n + from1; i++) {
             if (!in[i]) {
@@ -41,15 +41,15 @@ public:
         return cnt == n;
     }
     //求完成顺序,如果完不成，返回空集
-    //多少个点（点的最大消耗）,前、后关系数组，是否从1开始
-    vector<int> Orders(int n, vector<vector<int>>& edges, bool from1 = true) {
+    //多少个点,后、前关系数组，是否从1开始
+    vector<int> Orders(int n, vector<vector<int>>& edges, bool from1 = 0) {
         queue<int> q;
         vector<vector<int>> g(n + from1);
         vector<int> in(n + from1);
         vector<int> orders;
         for (auto& e : edges) {
-            g[e[1]].push_back(e[0]);
-            in[e[0]]++;
+            g[e[1]].push_back(e[0]);//前置点加与后置点的关系。
+            in[e[0]]++;//后置点加入度
         }
         for (int i = from1; i < n + from1; i++) {
             if (!in[i]) {
@@ -75,9 +75,9 @@ public:
         }
         return cnt == n ? orders : vector<int>();
     }
-    //求最小消耗（所有已知边消耗为1）
-    // 多少个点（点的最大消耗）,前、后的关系数组，是否从1开始
-    int MinCostDefault1(int n, vector<vector<int>>& edges, bool from1 = true) {
+    //每条边消耗为1，求最小消耗（所有已知边消耗为1）
+    // 多少个点,后、前关系数组，是否从1开始
+    int MinCostDefault1(int n, vector<vector<int>>& edges, bool from1 = 0) {
         bool IsNoCircle;
         int MinCost;
         vector<int> orders;
@@ -87,8 +87,8 @@ public:
         vector<int> in(n + from1);
         vector<int> maxCost(n + from1, 0);
         for (auto& e : edges) {
-            g[e[1]].push_back({ e[0],1 });
-            in[e[0]]++;
+            g[e[1]].push_back({ e[0],1 });//前置点加与后置点的关系。
+            in[e[0]]++;//后置点加入度
         }
         for (int i = from1; i < n + from1; i++) {
             if (!in[i]) {
@@ -121,9 +121,9 @@ public:
         return MinCost;
     }
 
-    //求是否无环，完成顺序，最小消耗
-    // 多少个点（点的最大消耗）,前、后、消耗的关系数组，是否从1开始
-    int MinCost(int n, vector<vector<int>>& edges, bool from1 = true) {
+    //每条边不同消耗,求是否无环，完成顺序，最小消耗
+    // 多少个点,后、前、消耗的关系数组，是否从1开始
+    int MinCost(int n, vector<vector<int>>& edges, bool from1 = 0) {
         bool IsNoCircle;
         int MinCost;
         vector<int> orders;
@@ -133,8 +133,8 @@ public:
         vector<int> in(n + from1);
         vector<int> maxCost(n + from1, 0);
         for (auto& e : edges) {
-            g[e[1]].push_back({ e[0],e[2] });
-            in[e[0]]++;
+            g[e[1]].push_back({ e[0],e[2] });//前置点加与后置点的关系。
+            in[e[0]]++;//后置点加入度
         }
         for (int i = from1; i < n + from1; i++) {
             if (!in[i]) {
@@ -166,9 +166,9 @@ public:
         }
         return MinCost;
     }
-    //求是否无环，完成顺序，最小消耗
-    // 多少个点（点的最大消耗），前后关系数组，每个点的用时，是否从1开始
-    int MinCost(int n, vector<vector<int>>& edges, vector<int>& time,bool from1=true) {
+    //每个点不同消耗。求是否无环，完成顺序，最小消耗
+    // 多少个点，后、前关系数组，每个点的用时消耗，是否从1开始
+    int MinCost(int n, vector<vector<int>>& edges, vector<int>& time,bool from1=0) {
         bool IsNoCircle;
         int MinCost;
         vector<int> orders;
@@ -177,8 +177,8 @@ public:
         vector<int> in(n + from1);
         vector<int> maxCost(n + from1);
         for (auto& e : edges) {
-            g[e[1]].push_back(e[0]);
-            in[e[0]]++;
+            g[e[1]].push_back(e[0]);//前置点加与后置点的关系。
+            in[e[0]]++;//后置点加入度
         }
         for (int i = from1; i < n + from1; i++) {
             if (!in[i]) {
@@ -211,7 +211,45 @@ public:
         }
         return MinCost;
     }
+    //求前后点间是否有前置关系,
+    //多少个点,前、后关系数组，是否从1开始
+    vector<vector<bool>> IsPre(int n, vector<vector<int>>& edges, bool from1 = 0) {
+        queue<int> q;
+        vector<vector<int>> g(n + from1);
+        vector<int> in(n + from1);
+        vector<vector<bool>> isPre(n, vector<bool>(n, false));
+        for (auto& e : edges) {
+            g[e[0]].push_back(e[1]);//前置点加与后置点的关系。
+            in[e[1]]++;//后置点加入度
+        }
+        for (int i = from1; i < n + from1; i++) {
+            if (!in[i]) {
+                q.push(i);
+            }
+        }
+        int cnt = 0;
+        while (!q.empty()) {
+            int len = q.size();
+            while (len--)
+            {
+                int cur = q.front();
+                //cur的顺序就是拓扑排序的顺序
+                cnt++;
+                q.pop();
+                for (auto& e : g[cur]) {
 
+                    isPre[cur][e] = true;
+                    for (int i = 0; i < n; ++i) {
+                        isPre[i][e] = isPre[i][e] || isPre[i][cur];
+                    }
+                    in[e]--;
+                    if (!in[e])
+                        q.push(e);
+                }
+            }
+        }
+        return isPre;
+    }
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
         return MinCost(n, relations, time);
     }
